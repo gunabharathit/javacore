aae600be535bfdd07111d3f18eb051f10dbbc352
gunabharathit <gunabharathi98@gmail.com>
2018-05-30 10:20:08 +0530

    if else program

---
+++ javacore/src/age.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class age {
+	public static void main(String args[])
+	{
+		int money=50;
+		if(money<=65)
+			System.out.println("money is less than or equal to 65");
+		else
+			System.out.println("money is greater than 65");
+	}
+
+
+}
+
