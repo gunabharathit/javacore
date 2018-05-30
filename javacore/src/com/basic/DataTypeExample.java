ccff49e354610b48a335d3ae40117f8373ca185d
gunabharathit <gunabharathi98@gmail.com>
2018-05-30 11:08:25 +0530

    While

---
+++ javacore/src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[] args) 
+	{
+		String firstName= "guna";
+		String lastName= "bharu";
+		System.out.println(firstName+" "+ lastName);
+	}
+
+}
---
+++ javacore/src/com/basic/error.java
@@ -0,0 +1,20 @@
+
+package com.basic;
+
+public class error {
+	public static void main(String args[])
+{
+	int total=0;
+	int b=1;
+		int y;
+	while(b<=10)
+	{
+		y=b*b;
+		System.out.println(y);
+		total +=y;
+		++b;
+		
+	}
+}
+
+}
